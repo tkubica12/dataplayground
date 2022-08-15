6780e621fbf86aeb4289bfcf06cc375eec40dd12
Tomas Kubica <tkubica12@gmail.com>
2022-08-15 21:32:41 +0200

    event hub capture

--- terraform/modules/data_lake/eventhub.tf
+++ terraform/modules/data_lake/eventhub.tf
@@ -12,6 +12,18 @@ resource "azurerm_eventhub" "pageviews" {
   resource_group_name = var.resource_group_name
   partition_count     = 2
   message_retention   = 1
+
+  capture_description {
+    enabled = true
+    encoding = "Avro"
+    interval_in_seconds = 60
+    destination {
+      storage_account_id = azurerm_storage_account.main.id
+      blob_container_name = azurerm_storage_container.bronze.name
+      name = "EventHubArchive.AzureBlockBlob"
+      archive_name_format = "pageviews_from_capture/{Namespace}/{EventHub}/{PartitionId}/{Year}/{Month}/{Day}/{Hour}/{Minute}/{Second}"
+    }
+  }
 }
 
 resource "azurerm_eventhub_authorization_rule" "pageviewsSender" {
