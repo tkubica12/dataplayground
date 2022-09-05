5091ed20a3f7b081051389984f582d883a36a654
Tomas Kubica <tkubica12@gmail.com>
2022-09-05 20:26:33 +0200

    final

--- terraform/modules/databricks/sql.tf
+++ terraform/modules/databricks/sql.tf
@@ -15,8 +15,7 @@ resource "databricks_sql_endpoint" "main" {
 resource "databricks_sql_global_config" "msin" {
   security_policy = "DATA_ACCESS_CONTROL"
   data_access_config = {
-    "spark.hadoop.fs.azure.account.key.${var.storage_account_name}.dfs.core.windows.net" : "${data.azurerm_storage_account.data_lake.primary_access_key}"
-    "spark.hadoop.fs.azure.account.auth.type.${var.storage_account_name}.dfs.core.windows.net" : "SharedKey"
+    
   }
   sql_config_params = {
     "ANSI_MODE" : "true"
--- terraform/modules/databricks/stores_and_tables.tf
+++ terraform/modules/databricks/stores_and_tables.tf
@@ -47,7 +47,7 @@ resource "databricks_schema" "main" {
   name         = "mydb"
 }
 
-// Storage access
+// Storage access - connector
 resource "azapi_resource" "access_connector" {
   type                      = "Microsoft.Databricks/accessConnectors@2022-04-01-preview"
   name                      = var.name_prefix
@@ -63,12 +63,33 @@ resource "azapi_resource" "access_connector" {
   })
 }
 
+// Storage access - managed identity credentials (for direct access to raw data)
+resource "databricks_storage_credential" "external_mi" {
+  name = "mi_credential"
+  azure_managed_identity {
+    access_connector_id = azapi_resource.access_connector.id
+  }
+  depends_on = [
+    databricks_metastore_assignment.main
+  ]
+}
+
+resource "databricks_grants" "external_creds" {
+  storage_credential = databricks_storage_credential.external_mi.id
+  grant {
+    principal  = "account users"
+    privileges = ["READ_FILES"]
+  }
+}
+
+// RBAC right to storage for managed identity
 resource "azurerm_role_assignment" "main" {
   scope                = data.azurerm_storage_account.data_lake.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azapi_resource.access_connector.identity[0].principal_id
 }
 
+// Metastore to use managed identity access connector
 resource "databricks_metastore_data_access" "main" {
   metastore_id = databricks_metastore.main.id
   name         = "dataaccess"
