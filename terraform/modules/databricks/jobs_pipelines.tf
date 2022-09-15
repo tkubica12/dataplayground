5d04330f27f27bcba283906bffd856aed09e1514
Tomas Kubica <tkubica12@gmail.com>
2022-09-15 10:18:41 +0200

    engagement table job

---
+++ terraform/modules/databricks/jobs_pipelines.tf
@@ -0,0 +1,35 @@
+// Streaming pipelines
+resource "databricks_pipeline" "pageviews" {
+  name    = "pageviews"
+  storage = "/mycatalog"
+  target = "mydb"
+
+  cluster {
+    label       = "default"
+    num_workers = 1
+  }
+
+  library {
+    notebook {
+      path = databricks_notebook.delta_live_stream.id
+    }
+  }
+
+  continuous = false
+}
+
+// jobs
+resource "databricks_job" "engagement_table" {
+  name = "engagement_table"
+
+  existing_cluster_id = databricks_cluster.etl_cluster.id
+
+  schedule {
+    quartz_cron_expression = "0 */30 * ? * *"
+    timezone_id            = "UTC"
+  }
+
+  notebook_task {
+    notebook_path = databricks_notebook.create_engagement_table.id
+  }
+}
