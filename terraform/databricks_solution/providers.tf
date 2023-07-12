1554e10a834e78ef7d2150f31ae6ca4f8aa83dde
tkubica12 <tkubica12@gmail.com>
2023-07-12 12:55:26 +0200

    test

--- terraform/databricks_solution/providers.tf
+++ terraform/databricks_solution/providers.tf
@@ -18,7 +18,14 @@ terraform {
     }
   }
 
-  backend "local" {}
+  backend "azurerm" {
+    resource_group_name  = "base"
+    storage_account_name = "tkubicastore"
+    container_name       = "tfstate"
+    key                  = "dataplayground.databricks.tfstate"
+    subscription_id      = "d3b7888f-c26e-4961-a976-ff9d5b31dfd3"
+    use_oidc             = true
+  }
 }
 
 provider "azurerm" {
